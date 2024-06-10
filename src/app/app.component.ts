a0586dfff81676091e98130f8cac75a791542152
Naveen Varma <naveenvarma9966@gmail.com>
2024-06-10 06:36:56 +0530

    Created User Component and Added user assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
 
 @Component({
-    selector: 'easy-task-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent]
+  selector: 'easy-task-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, HeaderComponent, UserComponent],
 })
-export class AppComponent {
-  
-}
+export class AppComponent {}
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'easy-task-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {}
